fbbe06ac0142b7a9bb7efe2a52fd0e5273e86dc1
Daniel T. Andreasen <daniel.andreasen@astro.up.pt>
2022-12-13 12:09:04 +0100

    Add app and flask test

--- src/astrolog/web/app.py
+++ src/astrolog/web/app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+import os
+
+from flask import Flask, flash, render_template
+
+from astrolog.database import Session
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello world!'
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+
+@app.route('/session/<int:session_id>')
+def session(session_id):
+    session = Session.get_or_none(session_id)
+    if not session:
+        flash(f'Session with id {session_id} was not found', category='warning')
+    return render_template('session.html', session=session)
 
 
 if __name__ == '__main__':
--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,20 +1,80 @@
+import datetime
+
 from flask_unittest import ClientTestCase
 
+from astrolog.database import (MODELS, EyePiece, Filter, Location, Object,
+                               Observation, Session, Telescope, db)
 from astrolog.web.app import app
 
 
-class TestFoo(ClientTestCase):
+def get_standard_session() -> Session:
+    horsens, _ = Location.get_or_create(name='Horsens', country='Denmark', latitude='55:51:38', longitude='-9:51:1', altitude=0)
+    september_13_1989 = datetime.datetime(1989, 9, 13).date()
+    session, _ = Session.get_or_create(date=september_13_1989, location=horsens)
+    return session
+
+
+def add_observation(session: Session) -> None:
+    orion_nebula, _ = Object.get_or_create(name='M42', magnitude=5.42)
+    moon, _ = Object.get_or_create(name='Moon', magnitude=-14)
+    telescope, _ = Telescope.get_or_create(name='Explorer 150P', aperture=150, focal_length=750)
+    plossl, _ = EyePiece.get_or_create(type='Plössl', focal_length=6, width=1.25)
+    kellner, _ = EyePiece.get_or_create(type='Kellner', focal_length=15, width=1.25)
+    moon_filter, _ = Filter.get_or_create(name='Moon filter')
+    Observation(object=orion_nebula, session=session, telescope=telescope, eyepiece=plossl).save()
+    Observation(object=moon, session=session, telescope=telescope, eyepiece=kellner, optic_filter=moon_filter).save()
+
+
+class TestApp(ClientTestCase):
     app = app
 
-    def test_simple_call(self, client):
-        rv = client.get('/hello')
-        self.assertStatus(rv, 200)
-        self.assertEqual(rv.data.decode(), 'Hello world!')
+    def tearDown(self, *args) -> None:
+        db.drop_tables(MODELS)
 
-    # def test_bar_with_client(self, client):
-    #     # Use the client here
-    #     # Example login request (on a hypothetical app)
-    #     rv = client.post('/login', {'username': 'pinkerton', 'password': 'secret_key'})
-    #     # Make sure rv is a redirect request to index page
-    #     self.assertLocationHeader('http://localhost/')
-    #     # Make sure session is set
+    def test_landing_page(self, client):
+        response = client.get('/')
+        self.assertStatus(response, 200)
+        self.assertInResponse(b'AstroLog</a>', response)
+        self.assertInResponse(b'All sessions</a>', response)
+        self.assertInResponse(b'Telescopes</a>', response)
+        self.assertInResponse(b'Equipment</a>', response)
+
+    def test_get_session(self, client):
+        response = client.get('/')
+        self.assertStatus(response, 200)
+        self.assertInResponse(b'AstroLog', response)
+        self.assertInResponse(b'<button class="btn btn-outline-primary" href="#">Get session</button>', response)
+
+        session = get_standard_session()
+        loc = session.location
+        response = client.get('/session/1')
+        self.assertStatus(response, 200)
+        self.assertInResponse(b'AstroLog', response)
+        self.assertInResponse(b'Session', response)
+        self.assertInResponse(f'Session - <small>{session.date.strftime("%d/%m/%Y")}</small>'.encode(), response)
+        self.assertInResponse(f'Location - <small title="Lat.: {loc.latitude}, Lon.: {loc.longitude}, Alt.: {loc.altitude}m">{session.location.name}</small>'.encode(), response)
+
+        add_observation(session)
+        response = client.get('/session/1')
+        self.assertInResponse(b'<th>Object</th>', response)
+        self.assertInResponse(b'<th>Magnitude</th>', response)
+        self.assertInResponse(b'<th>Telescope</th>', response)
+        self.assertInResponse(b'<th>Eyepiece</th>', response)
+        self.assertInResponse(b'<th>Magnification</th>', response)
+        self.assertInResponse(b'<th>Filter</th>', response)
+        self.assertInResponse(b'<th>Note</th>', response)
+        for observation in session.observations:
+            object_name = observation.object.name.encode()
+            magnitude = str(observation.object.magnitude).encode()
+            telescope_info = f'{observation.telescope.name} (f{observation.telescope.f_ratio})'.encode()
+            eyepiece = observation.eyepiece.type.encode()
+            magnification = f'{observation.magnification}X'.encode()
+            filter_ = observation.optic_filter.name.encode() if observation.optic_filter else ''.encode()
+            note = observation.note.encode() if observation.note else ''.encode()
+            self.assertInResponse(object_name, response)
+            self.assertInResponse(magnitude, response)
+            self.assertInResponse(telescope_info, response)
+            self.assertInResponse(eyepiece, response)
+            self.assertInResponse(magnification, response)
+            self.assertInResponse(filter_, response)
+            self.assertInResponse(note, response)
