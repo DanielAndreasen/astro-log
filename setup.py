e8e346e659c56391dde36866fea4302b0bef6c28
Daniel T. Andreasen <daniel.andreasen@astro.up.pt>
2023-04-08 15:17:55 +0200

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='astrolog',
-    version='0.0.1',
+    version='1.0.0',
 
     packages=find_packages('src'),
     package_dir={'': 'src'},
